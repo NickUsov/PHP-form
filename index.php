9417e6f32429664d127bd97056d06382fd00d8f2
Student <student@kr.step.ua>
2019-09-05 20:53:24 +0300

    go home

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        li {
+            list-style-type:none;
+        }
+        a {
+            text-decoration: none;
+            color: gray;
+        }
+    </style>
+</head>
+<body>
+    <ul>
+        <li><a href="addUser.php">Add</a></li>
+        <li><a href="showUser.php">Show</a></li>
+    </ul>
+</body>
+</html>
